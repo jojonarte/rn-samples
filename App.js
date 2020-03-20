a4021b93628042f853a2617b8a12fa12e311b40e
jojonarte <joselitonarte@gmail.com>
2020-03-21 01:04:02 +0800

    basic form sample/

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
-/**
- * This is a demo of a react-native input
- * The most basic input component for react-native is the TextInput
- *
- *	First text input is an uncontrolled one
- * The second is a controlled input
- * If you remove line 30
- * then try typing anything into it in the simulator/emulator
- * you will not see changes appear to the input value
- */
 export default class App extends React.Component {
 	state = {
-		controlledValue: ''
+		name: '',
+		phone: ''
 	};
-	handleInputChange = newValue => this.setState({ controlledValue: newValue });
+	handleNameChange = name => this.setState({ name });
+	handlePhoneNumChange = phone => this.setState({ phone });
+	handleSubmit = () => {
+		// normally we can do the submission logic here or delegate the action in the upper scope
+		// we can discuss more about this in the future
+		// for now we log.
+		console.log('Input values', this.state);
+	};
 	render() {
 		return (
 			<View style={styles.container}>
 				<TextInput
 					style={styles.input}
-					placeholder="This is an uncontrolled input"
+					value={this.state.name}
+					placeholder="Name"
+					onChangeText={this.handleNameChange}
 				/>
 				<TextInput
 					style={styles.input}
-					value={this.state.controlledValue}
-					placeholder="This is a controlled input"
-					onChangeText={this.handleInputChange}
+					value={this.state.phone}
+					placeholder="Phone"
+					keyboardType="phone-pad"
+					onChangeText={this.handlePhoneNumChange}
 				/>
+				<Button title="Submit values" onPress={this.handleSubmit} />
 			</View>
 		);
 	}
