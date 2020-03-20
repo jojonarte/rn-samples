fb8d3b03a28316861b34329cde6feeab902e242b
jojonarte <joselitonarte@gmail.com>
2020-03-20 23:07:59 +0800

    different event handlers demo

--- App.js
+++ App.js
@@ -1,34 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	ScrollView,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	TouchableHighlight
+} from 'react-native';
 import Constants from 'expo-constants';
 
 export default class App extends React.Component {
-	/**
-	 * The button will not be clickable and the JS thread is blocked for 5 seconds
-	 * but you can interact with the whole screen or your whole simulator/emulator
-	 * while it's happening and no crash will happen.
-	 *
-	 * bouncing view when scrolling might only work on iOS devices/Simulators
-	 *
-	 * the logs will also be visible on the terminal where you run expo
-	 */
-	blockJSThread = () => {
-		const dateAfter5 = Date.now() + 5000;
-		console.log('start blocking');
-		while (Date.now() < dateAfter5) {
-			console.log('blocked JS but you can keep scrolling the view');
-		}
-		console.log('end blocking');
-	};
-
 	render() {
 		return (
 			<View style={styles.container}>
 				<ScrollView contentContainerStyle={styles.container}>
 					<Button
-						title="Press to block JS thread"
-						onPress={this.blockJSThread}
+						title="Button Component"
+						onPress={() => console.log('Button component pressed')}
 					/>
+					<TouchableWithoutFeedback
+						onPress={() =>
+							console.log('TouchableWithoutFeedback component pressed')
+						}
+					>
+						<View>
+							<Text> TouchableWithoutFeedback</Text>
+							<Text>
+								You have to wrap child components with a View as parent if there
+								are more than one child
+							</Text>
+							<Text>Try pressing on this and you will see no effect</Text>
+						</View>
+					</TouchableWithoutFeedback>
+					<TouchableHighlight
+						onPress={() => console.log('TouchableHighlight component pressed')}
+					>
+						<View>
+							<Text> TouchableHighlight</Text>
+							<Text>
+								You have to wrap child components with a View as parent if there
+								are more than one child
+							</Text>
+							<Text>
+								Try pressing on this and you will see a highlighting effect
+							</Text>
+						</View>
+					</TouchableHighlight>
+					<TouchableOpacity
+						onPress={() => console.log('TouchableOpacity component pressed')}
+					>
+						<Text>You can wrap anything just like a div</Text>
+						<Text>
+							Try pressing on this and you will see the text color will lighten
+						</Text>
+					</TouchableOpacity>
 				</ScrollView>
 			</View>
 		);
@@ -39,8 +66,9 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
+		flexDirection: 'column',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'space-around',
 		marginTop: Constants.statusBarHeight
 	}
 });
