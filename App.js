606d65dfcd01c9e899ffc9d9fac8d78f5109da01
jojonarte <joselitonarte@gmail.com>
2020-03-21 01:15:02 +0800

    keyboard avoiding view sample

--- App.js
+++ App.js
@@ -1,65 +1,33 @@
 import React from 'react';
-import { StyleSheet, TextInput, View, Button, Alert } from 'react-native';
+import {
+	StyleSheet,
+	TextInput,
+	View,
+	Button,
+	Alert,
+	KeyboardAvoidingView
+} from 'react-native';
 
+/**
+ * Try replacing KeyboardAvoidingView view below with View
+ * then compare the app behavior
+ *
+ * read more - https://reactnative.dev/docs/keyboardavoidingview
+ */
 export default class App extends React.Component {
-	state = {
-		name: '',
-		phone: '',
-		isFormValid: false
-	};
-	handleNameChange = name => this.setState({ name }, this.checkFormValidity);
-	handlePhoneNumChange = phone =>
-		this.setState({ phone }, this.checkFormValidity);
-
-	/**
-	 * sample live validation via this.setState callback
-	 * - most common pattern in mobile apps
-	 */
-	checkFormValidity = () => {
-		if (this.state.phone.length < 10 || !this.state.name) {
-			return this.setState({ isFormValid: false });
-		}
-
-		return this.setState({ isFormValid: true });
-	};
-
-	/**
-	 * example basic validation on submit
-	 */
-	handleSubmit = () => {
-		let isFormValid = false;
-		if (this.state.phone.length < 10) {
-			Alert.alert('error', 'Invalid phone number');
-		} else if (!this.state.name) {
-			Alert.alert('error', 'Invalid name');
-		} else {
-			console.log('submit success');
-			isFormValid = true;
-		}
-		this.setState({ isFormValid });
-	};
 	render() {
 		return (
-			<View style={styles.container}>
-				<TextInput
-					style={styles.input}
-					value={this.state.name}
-					placeholder="Name"
-					onChangeText={this.handleNameChange}
-				/>
-				<TextInput
-					style={styles.input}
-					value={this.state.phone}
-					placeholder="Phone"
-					keyboardType="phone-pad"
-					onChangeText={this.handlePhoneNumChange}
-				/>
-				<Button
-					title="Submit values"
-					onPress={this.handleSubmit}
-					disabled={!this.state.isFormValid}
-				/>
-			</View>
+			<KeyboardAvoidingView behavior="padding" style={styles.container}>
+				<TextInput style={styles.input} placeholder="Input 1" />
+				<TextInput style={styles.input} placeholder="Input 1" />
+				<TextInput style={styles.input} placeholder="Input 1" />
+				<TextInput style={styles.input} placeholder="Input 1" />
+				<TextInput style={styles.input} placeholder="Input 1" />
+				<TextInput style={styles.input} placeholder="Input 1" />
+				<TextInput style={styles.input} placeholder="Input 1" />
+
+				<Button title="Submit values" />
+			</KeyboardAvoidingView>
 		);
 	}
 }
@@ -75,6 +43,7 @@ const styles = StyleSheet.create({
 		borderColor: 'black',
 		borderWidth: 1,
 		width: 300,
-		padding: 10
+		padding: 10,
+		marginVertical: 24
 	}
 });
